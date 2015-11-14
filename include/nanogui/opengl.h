5d9b8c886dc585b8ecb4e5eaec7111a9194e4489
r-lyeh <r-lyeh@users.noreply.github.com>
2015-11-14 16:25:12 +0100

    Update opengl.h
    Do not redefine GLEW_STATIC if already present (ie, when embedding NanoGUI in an external project)

--- include/nanogui/opengl.h
+++ include/nanogui/opengl.h
@@ -17,7 +17,9 @@
 #if defined(__APPLE__)
     #define GLFW_INCLUDE_GLCOREARB
 #elif defined(_WIN32)
+    #ifndef GLEW_STATIC
     #define GLEW_STATIC
+    #endif
     #include <GL/glew.h>
 #else
     #define GL_GLEXT_PROTOTYPES
