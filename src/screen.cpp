e96a995d20150b020edce90dfec7c765a66f8799
Steve <steven.g.ball@gmail.com>
2016-07-25 15:24:54 -0400

    tabs to spaces

--- src/screen.cpp
+++ src/screen.cpp
@@ -253,12 +253,12 @@ void Screen::initialize(GLFWwindow *window, bool shutdownGLFWOnDestruct) {
 #endif
 
 #if defined(NANOGUI_GLAD)
-	if (!gladInitialized) {
-		gladInitialized = true;
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-			throw std::runtime_error("Could not initialize GLAD!");
-		glGetError(); // pull and ignore unhandled errors like GL_INVALID_ENUM
-	}
+    if (!gladInitialized) {
+        gladInitialized = true;
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+            throw std::runtime_error("Could not initialize GLAD!");
+        glGetError(); // pull and ignore unhandled errors like GL_INVALID_ENUM
+    }
 #endif
 
     /* Detect framebuffer properties and set up compatible NanoVG context */
