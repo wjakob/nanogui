cac1fe3ab8d1143c0bafae30862d7770d7dcf1f3
Roma Kovalev <roma.kvl@gmail.com>
2016-09-28 17:10:37 +0400

    Initialize glad before glViewport (fixes #136)

--- src/example3.cpp
+++ src/example3.cpp
@@ -78,16 +78,16 @@ int main(int /* argc */, char ** /* argv */) {
         return -1;
     }
     glfwMakeContextCurrent(window);
-
+    
+    // Create a nanogui screen and pass the glfw pointer to initialize
+    screen = new Screen();
+    screen->initialize(window, true);
+    
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     glfwSwapInterval(0);
     glfwSwapBuffers(window);
-
-    // Create a nanogui screen and pass the glfw pointer to initialize
-    screen = new Screen();
-    screen->initialize(window, true);
 
     // Create nanogui gui
     bool enabled = true;
