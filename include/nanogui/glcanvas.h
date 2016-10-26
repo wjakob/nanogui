6a41308072eb75a8c1c561390218475282a01ff9
Jan Winkler <jan.winkler.84@gmail.com>
2016-10-26 21:15:27 +0200

    More elaborate description of GLCanvas

--- include/nanogui/glcanvas.h
+++ include/nanogui/glcanvas.h
@@ -1,7 +1,8 @@
 /*
     nanogui/glcanvas.h -- Canvas widget for rendering full-fledged
     OpenGL content within its designated area. Very useful for
-    displaying and manipulating 3D objects or scenes.
+    displaying and manipulating 3D objects or scenes. Subclass it and
+    overload `drawGL` for rendering.
 
     NanoGUI was developed by Wenzel Jakob <wenzel.jakob@epfl.ch>.
     The widget drawing code is based on the NanoVG demo application
