f9c395152f81986b2352c8353c37a9fd33e2457e
John Buckley <nhoj.buckley@gmail.com>
2017-08-10 10:24:21 +0100

    Removing use of -retain (incompatible with ARC). (#243)

--- src/darwin.mm
+++ src/darwin.mm
@@ -6,7 +6,7 @@ NAMESPACE_BEGIN(nanogui)
 std::string file_dialog(const std::vector<std::pair<std::string, std::string>> &filetypes, bool save) {
     std::string path = "";
     if (save) {
-        NSSavePanel *saveDlg = [[NSSavePanel savePanel] retain];
+        NSSavePanel *saveDlg = [NSSavePanel savePanel];
 
         NSMutableArray *types = [NSMutableArray new];
         for (size_t idx = 0; idx < filetypes.size(); ++idx)
@@ -17,7 +17,7 @@ std::string file_dialog(const std::vector<std::pair<std::string, std::string>> &
         if ([saveDlg runModal] == NSModalResponseOK)
             path = [[[saveDlg URL] path] UTF8String];
     } else {
-        NSOpenPanel *openDlg = [[NSOpenPanel openPanel] retain];
+        NSOpenPanel *openDlg = [NSOpenPanel openPanel];
 
         [openDlg setCanChooseFiles:YES];
         [openDlg setCanChooseDirectories:NO];
