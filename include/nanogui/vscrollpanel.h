55a09ef67386b74d79de6ab6142c4031f13fcba4
Thomas Müller <thomas94@gmx.net>
2017-09-01 18:42:26 +0200

    Allow accessing the scroll amount of VScrollPanel from outside

--- include/nanogui/vscrollpanel.h
+++ include/nanogui/vscrollpanel.h
@@ -27,6 +27,11 @@ class NANOGUI_EXPORT VScrollPanel : public Widget {
 public:
     VScrollPanel(Widget *parent);
 
+    /// Return the current scroll amount as a value between 0 and 1. 0 means scrolled to the top and 1 to the bottom.
+    float scroll() const { return mScroll; }
+    /// Set the scroll amount to a value between 0 and 1. 0 means scrolled to the top and 1 to the bottom.
+    void setScroll(float scroll) { mScroll = scroll; }
+
     virtual void performLayout(NVGcontext *ctx) override;
     virtual Vector2i preferredSize(NVGcontext *ctx) const override;
     virtual bool mouseDragEvent(const Vector2i &p, const Vector2i &rel, int button, int modifiers) override;
