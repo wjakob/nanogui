1ebe152fe533a027b61bcbdddb1f6802178e841a
s.kushnirenko <s.kushnirenko@gaijin.ru>
2020-02-08 15:25:43 +0300

    render: dx12: fixed create window/context

--- src/dx12/common_dx12.cpp
+++ src/dx12/common_dx12.cpp
@@ -7,8 +7,8 @@ Port to dx12 by megai2
 
 #if NANOGUI_DX12_BACKEND
 
-#include <windowsx.h>
-#include <winerror.h>
+#include "common_dx12.h"
+#include "dx12_subset.h"
 
 #  ifndef NOMINMAX
 #    define NOMINMAX
@@ -19,11 +19,24 @@ Port to dx12 by megai2
 #    define WIN32_LEAN_AND_MEAN
 #  endif
 
+#define NANOVG_D3D12_IMPLEMENTATION
+#define INITGUID
+#include <nanogui/nanovg_d3d12.h>
+#include <windows.h>
+#include <windowsx.h>
+
 NAMESPACE_BEGIN(nanogui)
 
-extern HINSTANCE hInstance;
-extern HWND hWndmain;
-extern const char* pszWindowClass;
+HINSTANCE hInstance;
+HWND hWndmain;
+dx12_subset pDX12;
+bool dxPremultMode = false;
+const char* pszWindowClass = "WindowClass";
+
+int dx12mouseposx = 0;
+int dx12mouseposy = 0;
+int xWin = 0;
+int yWin = 0;
 
 
 enum {
@@ -55,6 +68,19 @@ void dx12SetDropCallback(const std::function<void(void *, int , const char **)> 
 void dx12SetScrollCallback(const std::function<void(void *, double , double )> &f) { scrollCallback = f; }
 void dx12SetFramebufferSizeCallback(const std::function<void(void *, int, int)> &f) { resizeCallback = f; }
 void dx12SetWindowFocusCallback(const std::function<void(void *, int )> &f) { focusCallback = f; }
+
+// Frees everything
+void UnInitializeDX()
+{
+  pDX12.deinit();
+}
+
+HRESULT resizeWindow(unsigned int x, unsigned int y)
+{
+  HRESULT hr = S_OK;
+  pDX12.set_size(x, y);
+  return hr;
+}
 
 void dx12SetCursorImage(HWND window, HCURSOR cursor)
 {
@@ -200,21 +226,18 @@ const char* dx12GetClipboardString(HWND window)
   return clipboardString;
 }
 
-bool appPostEmptyEvent(void)
-{
-  PostMessage(hWndmain, WM_NULL, 0, 0); return false;
-}
+bool sample::post_empty_event(void) { PostMessage(hWndmain, WM_NULL, 0, 0); return false; }
 
-bool appWaitEvents(void)
+bool sample::wait_events(void)
 {
   WaitMessage();
 
-  appPollEvents();
+  sample::poll_events();
   return false;
 }
 
 static bool dx12showCloseScreen = false;
-bool appPollEvents(void)
+bool sample::poll_events(void)
 {
   MSG msg;
   HWND handle;
@@ -501,6 +524,206 @@ float getTimeFromStart(void)
     QueryPerformanceCounter((LARGE_INTEGER*)&time);
     return (time - dx12startTime)/1000000.f;
 }
+
+// Setup the device and the rendering targets
+BOOL InitializeDX(void* window, unsigned int x, unsigned int y)
+{
+  hWndmain = (HWND)window;
+  pDX12.init((HWND)window, x, y);
+  resizeWindow(x, y);
+
+  return true;
+}
+
+void setWindowSize(HWND window, int w, int h)
+{
+  RECT rect = { 0, 0, w, h };
+  AdjustWindowRectEx(&rect, WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+    FALSE, WS_EX_APPWINDOW);
+  SetWindowPos((HWND)window, HWND_TOP,
+    0, 0, rect.right - rect.left, rect.bottom - rect.top,
+    SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOMOVE | SWP_NOZORDER);
+}
+
+sample::WindowHandle sample::create_window(int w, int h, const std::string& caption, bool resizable, bool fullscreen)
+{
+  RECT rcWin;
+  sample::WindowHandle hw_window;
+
+  if (fullscreen) {
+    //???
+  }
+  else
+  {
+    rcWin.left = 0;
+    rcWin.right = w;
+    rcWin.top = 0;
+    rcWin.bottom = h;
+
+    AdjustWindowRectEx(&rcWin, WS_OVERLAPPEDWINDOW, FALSE, 0);
+
+    rcWin.right += -rcWin.left;
+    rcWin.bottom += -rcWin.top;
+
+    hw_window = (void*)CreateWindowEx(0, pszWindowClass, (caption + " (DX11)").c_str(), WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, CW_USEDEFAULT, (int)rcWin.right, (int)rcWin.bottom, NULL, NULL, hInstance, NULL);
+  }
+
+  if (!hw_window)
+    throw std::runtime_error("Could not create an Window ");
+
+  float pixelRatio = get_pixel_ratio((HWND)hw_window);
+  int ww = w * pixelRatio;
+  int hh = h * pixelRatio;
+  if (pixelRatio != 1 && !fullscreen)
+    setWindowSize((HWND)hw_window, ww, hh);
+
+  ShowWindow((HWND)hw_window, SW_SHOW);
+
+  if (FAILED(InitializeDX(hw_window, ww, hh)))
+    throw std::runtime_error("Could not create an dx11 context ");
+
+  return hw_window;
+}
+
+void sample::present_frame(void* window) { pDX12.fr_end(); }
+void sample::clear_frame(Color background) 
+{
+  if (!pDX12.isRunning)
+    return;
+
+  pDX12.fr_start(); 
+}
+
+/* Calculate pixel ratio for hi-dpi devices. */
+static float get_pixel_ratio(HWND window) {
+  HMONITOR monitor = nullptr;
+  static HMONITOR(WINAPI *MonitorFromWindow_)(HWND, DWORD);
+  static bool MonitorFromWindow_tried = false;
+  if (!MonitorFromWindow_tried) {
+    auto user32 = LoadLibrary(TEXT("user32"));
+    if (user32)
+      MonitorFromWindow_ = (decltype(MonitorFromWindow_))GetProcAddress(user32, "MonitorFromWindow");
+    MonitorFromWindow_tried = true;
+  }
+  if (MonitorFromWindow_)
+    monitor = MonitorFromWindow_(hWndmain, 2);
+  /* The following function only exists on Windows 8.1+, but we don't want to make that a dependency */
+  static HRESULT(WINAPI *GetDpiForMonitor_)(HMONITOR, UINT, UINT*, UINT*) = nullptr;
+  static bool GetDpiForMonitor_tried = false;
+
+  if (!GetDpiForMonitor_tried) {
+    auto shcore = LoadLibrary(TEXT("shcore"));
+    if (shcore)
+      GetDpiForMonitor_ = (decltype(GetDpiForMonitor_))GetProcAddress(shcore, "GetDpiForMonitor");
+    GetDpiForMonitor_tried = true;
+  }
+
+  if (GetDpiForMonitor_ && monitor) {
+    uint32_t dpiX, dpiY;
+    if (GetDpiForMonitor_(monitor, 0 /* effective DPI */, &dpiX, &dpiY) == S_OK)
+      return dpiX / 96.0;
+  }
+  return 1.f;
+}
+
+void sample::create_context() {
+  __nanogui_context = nvgCreateD3D12(&pDX12, NVG_ANTIALIAS | NVG_STENCIL_STROKES);
+  if (__nanogui_context == nullptr)
+    throw std::runtime_error("Could not initialize NanoVG!");
+}
+
+void sample::destroy_window(sample::WindowHandle) { UnInitializeDX(); }
+
+
+void sample::setup_window_params(WindowHandle hw_window, Screen* screen)
+{
+  /* Propagate GLFW events to the appropriate Screen instance */
+  static Screen* s = screen;
+  dx12SetCursorPosCallback([](void *w, double x, double y) {
+    dx12mouseposx = x;
+    dx12mouseposy = y;
+    if (!s->canProcessEvents())
+      return;
+    s->cursorPosCallbackEvent(x, y);
+  });
+
+  dx12SetMouseButtonCallback([](void *w, int button, int action, int modifiers) {
+    if (!s->canProcessEvents())
+      return;
+    s->mouseButtonCallbackEvent(button, action, modifiers);
+  });
+
+  dx12SetKeyCallback([](void *w, int key, int scancode, int action, int mods) {
+    if (!s->canProcessEvents())
+      return;
+    s->keyCallbackEvent(key, scancode, action, mods);
+  });
+
+  dx12SetCharCallback([](void *w, unsigned int codepoint) {
+    if (!s->canProcessEvents())
+      return;
+    s->charCallbackEvent(codepoint);
+  });
+
+  dx12SetDropCallback([](void *w, int count, const char **filenames) {
+    if (!s->canProcessEvents())
+      return;
+    s->dropCallbackEvent(count, filenames);
+  });
+
+  dx12SetScrollCallback([](void *w, double x, double y) {
+    if (!s->canProcessEvents())
+      return;
+    s->scrollCallbackEvent(x, y);
+  });
+
+  dx12SetFramebufferSizeCallback([](void *w, int width, int height) {
+    if (!s->canProcessEvents())
+      return;
+    s->resizeCallbackEvent(width, height);
+  });
+
+  // notify when the screen has lost focus (e.g. application switch)
+  dx12SetWindowFocusCallback([](void *w, int focused) {
+    // focused: 0 when false, 1 when true
+    s->focusEvent(focused != 0);
+  });
+
+  s->initClipboardSetHandler([hw_window](std::string text) { dx12SetClipboardString((HWND)hw_window, text.c_str()); });
+  s->initClipboardGetHandler([hw_window]() { return dx12GetClipboardString((HWND)hw_window); });
+
+  s->setPrepareFrameHandler([hw_window](Screen* screen) {
+    Vector2i fbsize, size;
+    dx12GetWindowSize((HWND)hw_window, &size[0], &size[1]);
+    screen->setSizeFBSize(size, size);
+  });
+
+  s->setResizeHwHandler([hw_window](Screen*, Vector2i& size, Vector2i& fbsize) {
+    dx12GetWindowSize((HWND)hw_window, &size[0], &size[1]);
+    fbsize = size;
+  });
+
+  s->initHwCursorSetter([hw_window](intptr_t cursor) { dx12SetCursorImage((HWND)hw_window, (HCURSOR)cursor); });
+  s->setPixelRatio(get_pixel_ratio((HWND)hw_window));
+}
+
+LPSTR translateCursorShape(int shape)
+{
+  switch (shape)
+  {
+  case 0: return IDC_ARROW;
+  case 1: return IDC_IBEAM;
+  case 2: return IDC_CROSS;
+  case 3: return IDC_HAND;
+  case 4: return IDC_SIZEWE;
+  case 5: return IDC_SIZENS;
+  }
+
+  return nullptr;
+}
+intptr_t __nanogui_create_cursor(int shape) { return (intptr_t)CopyCursor(LoadCursorA(NULL, translateCursorShape(shape))); }
+
 
 NAMESPACE_END(nanogui)
 
